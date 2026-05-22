3e27d346744cca882c5bac3245ab119a7653d6d2
mirzoyantigran61-cell <mirzoyantigran61@gmail.com>
2026-05-22 19:17:02 +0400

    Create sw.js

---
+++ client/public/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'shop-platform-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('push', event => {
+  const options = {
+    body: event.data.text(),
+    icon: '/icons/icon-192x192.png',
+    badge: '/icons/icon-72x72.png',
+    vibrate: [200, 100, 200],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: 1
+    },
+    actions: [
+      { action: 'explore', title: 'View Details', icon: '/icons/checkmark.png' },
+      { action: 'close', title: 'Close', icon: '/icons/xmark.png' }
+    ]
+  };
+  
+  event.waitUntil(
+    self.registration.showNotification('Shop Platform', options)
+  );
+});
